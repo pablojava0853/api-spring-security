b0b6011b86b776865be0f78cb2b3a542fdaad0fb
Pablo Gonzalez <pablojava853@outlook.com>
2021-06-24 16:56:23 -0300

    Commit inicial

---
+++ src/main/java/br/com/pgs/apispringsecurity/entity/VendaPagamento.java
@@ -0,0 +1,21 @@
+package br.com.pgs.apispringsecurity.entity;
+import lombok.Data;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name="venda_pagamento")
+public class VendaPagamento {
+	@Id
+    @SequenceGenerator(name = "venda_pagamento_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "venda_pagamento_sequence")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_tipo_pagamento")
+    private TipoPagamento pagamento;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+}
